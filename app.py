9a0f0b5e9b10eaf26cb8e0b1f4e3dccd3e9829df
rubab05 <111947291+rubab05@users.noreply.github.com>
2023-10-15 23:05:21 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pickle
 from flask import Flask, render_template, request
 
 # Initialize Flask app
-app = Flask(__name__)
+app = Flask(__name)
 
 # Load the saved sentiment analysis model from the .pkl file
 with open('sentiment_model.pkl', 'rb') as model_file:
@@ -14,6 +14,7 @@ with open('vectorizer.pkl', 'rb') as vectorizer_file:
 
 # Define a function for sentiment analysis
 def predict_sentiment(text):
+
     # Vectorize the text using the loaded CountVectorizer
     text_vectorized = vectorizer.transform([text])
 
@@ -23,9 +24,11 @@ def predict_sentiment(text):
     # Return the result as 'positive' or 'negative'
     return 'positive' if prediction[0] == 1 else 'negative'
 
+
 # Define a route for the home page
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
     result = None
 
     if request.method == 'POST':
@@ -33,6 +36,7 @@ def index():
         result = predict_sentiment(text)
 
     return render_template('index.html', result=result)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
